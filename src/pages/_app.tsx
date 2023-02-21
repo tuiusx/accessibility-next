7351a2f2cc191723dbaa82ea27c03c81b838d729
tuiusx <tuiusx@icloud.com>
2023-02-20 21:44:56 -0300

    configuring axe-core

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { axeAccessibilityReporter } from '@/utils/axeAccessibilityReporter'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter()
+  }, [])
+
   return <Component {...pageProps} />
 }
